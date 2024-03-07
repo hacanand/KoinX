fc69ef2b20372b3dc6cd4453a815d6eda964b0dc
anand <anandpromax@gmail.com>
2024-03-07 16:43:20 +0530

    Refactor layout in App.js and made right component visible in sm devices

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import TrendingCryptoDashboard from "./components/TrendingCryptoDashboard";
 import CryptoInfoCard from "./components/CryptoInfoChart";
-import InformationSection from "./components/InformationSection";
+import InformationSection from "./components/InformationSection"; 
 import Navbar from "./components/Navbar";
 import NavigationBarDashboard from "./components/NavigationBarDashboard";
 import RightCard from "./components/RightCard";
@@ -14,10 +14,11 @@ import NavigationBar from "./helper/NavigationBar";
 function App() {
   return (
     <div className="h-full w-full bg-slate-100">
+      <div className="flex flex-col">
         <Navbar />
         <Navigation />
-        <div className="md:flex m-1">
-          <div className="flex flex-col gap-6 md:mx-16">
+        <div className="md:flex m-1 md:m-0">
+          <div className="flex flex-col gap-6 md:ml-10">
             <CryptoInfoCard />
             <NavigationBar />
             <NavigationBarDashboard />
@@ -26,16 +27,16 @@ function App() {
             <Tokenomics />
             <TeamMembers />
           </div>
-          <div className=" flex-shrink-0 mr-16 gap-8 hidden md:block flex-col">
+          <div className="flex flex-col gap-6 md:mx-16 ">
             <RightCard />
             <TrendingCoins />
           </div>
         </div>
-        <div className="mt-16  ">
-          <TrendingCryptoDashboard />
-        </div>
       </div>
- 
+      <div className="mt-16  ">
+        <TrendingCryptoDashboard />
+      </div>
+    </div>
   );
 }
 
